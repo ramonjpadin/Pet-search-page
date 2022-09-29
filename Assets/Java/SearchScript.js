c1d18536d72a1cdd7430713dae530ce44d6f7130
Jon Beck <jonnydbeck@gmail.com>
2022-09-29 19:14:52 -0400

    Parameter Getting Added

--- Assets/Java/SearchScript.js
+++ Assets/Java/SearchScript.js
@@ -3,13 +3,47 @@ var apiCode = "pSIHbptoz8mLgn5KonemY0MUsxaC0nsu3rRhAD3Kz19RVhwXlv";
 //Query BTNs
 var advBtn1 = $('#advBtn1');
 var advBtn2 = $('#advBtn2');
+var normQuer = $('#normQuer')
 var advQuer = $('#advQuer');
 var supQuer = $('#supQuer');
 
 //Other BTNs
-var searchBtn = $('#supQuer');
+var searchBtn = $('#Quer');
 
 var advShow = false;
+var thisParams = getParams();
+autoSelect(thisParams);
+
+//Parameter Catching
+function getParams() {
+    var idx = document.URL.indexOf('?');
+    var params = new Array();
+    if (idx != -1) {
+    var pairs = document.URL.substring(idx+1, document.URL.length).split('&');
+    for (var i=0; i<pairs.length; i++) {
+    nameVal = pairs[i].split('=');
+    params[nameVal[0]] = nameVal[1];
+       }
+    }
+    return params;
+}
+
+function autoSelect(pams){
+    switch (pams.species){
+        case "dog":
+            $('#Species-1').prop( "checked", true );
+            break;
+        case "cat":
+            $('#Species-2').prop( "checked", true );
+            break;
+        case "small%20animals":
+            $('#Species-3').prop( "checked", true );
+            break;
+        case "other%20animals":
+            //$('#Species-1').prop( "checked", true );
+            break;
+    }
+}
 
 //These two functions control the query accordion
 advBtn1.click(function(e){
@@ -38,3 +72,19 @@ advBtn2.click(function(e){
 });
 
 //
+
+searchBtn.click(function(e){
+
+    var gQueryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + locale[0] + "&lon=" + locale[1] + "&appid=" + wAPIKey + "&units=imperial";
+
+    fetch(gQueryURL)
+    .then((response) => response.json())
+    .then(function (data){
+        console.log
+    })
+})
+
+//
+function CreateResult (img, name, species, breed){
+
+}
