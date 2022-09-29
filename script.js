5593cd17fbc11209635b1ac76078d163507dc788
Ramon Padin <ramonjpadin@gmail.com>
2022-09-28 20:57:04 -0500

    removed some things

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
-const portfolioItems = document.querySelector('.section');
+
 
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
 });
 
-
-
-portfolioItems.forEach(portfolioItem => {
-    portfolioItem.addEventListener('mouseover', () => {
-        portfolioItem.childNodes[1].classList.add('img-darken');
-    })
-
-    portfolioItem.addEventListener('mouseout', () => {
-        portfolioItem.childNodes[1].classList.remove('img-darken');
-    })
-})
