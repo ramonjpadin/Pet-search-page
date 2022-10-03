a49776f29233832f8e1cc87ae54220c1d059b487
Jon Beck <jonnydbeck@gmail.com>
2022-10-03 00:51:24 -0400

    Pet-Search Working

--- Assets/Java/SearchScript.js
+++ Assets/Java/SearchScript.js
@@ -1,11 +1,10 @@
 var apiCode = "pSIHbptoz8mLgn5KonemY0MUsxaC0nsu3rRhAD3Kz19RVhwXlv";
+var sApiCode = "jaUgYeFFgUvh3pKQdoHBtw0Iw9eDAXjwfmFfDhVt";
 
 //Query BTNs
 var advBtn1 = $('#advBtn1');
-var advBtn2 = $('#advBtn2');
 var normQuer = $('#normQuer')
 var advQuer = $('#advQuer');
-var supQuer = $('#supQuer');
 
 //Other BTNs
 var searchBtn = $('#searchBtn');
@@ -15,10 +14,6 @@ var petContainer = $('#petContainer');
 var advShow = false;
 var thisParams = getParams();
 autoSelect(thisParams);
-
-//test function
-//var guyImg = "https://www.cdc.gov/injury/images/pressroom/sme/GeryGuy_285x360.png";
-//CreateResult(guyImg, "Guy", "Person", "Dood", "none=none")
 
 //Parameter Catching
 function getParams() {
@@ -43,7 +38,7 @@ function autoSelect(pams){
             $('#Species-2').prop( "checked", true );
             break;
         case "small%20animals":
-            $('#Species-3').prop( "checked", true );
+            $('#Species-4').prop( "checked", true );
             break;
         case "other%20animals":
             //$('#Species-1').prop( "checked", true );
@@ -55,14 +50,11 @@ function autoSelect(pams){
 advBtn1.click(function(e){
     if(advShow){
         e.preventDefault();
-        advBtn2.addClass("hidden");
         advQuer.addClass("hidden");
-        supQuer.addClass("hidden");
         advBtn1.text("Show Advanced Options");
         advShow = false;
     }else{
         e.preventDefault();
-        advBtn2.removeClass("hidden");
         advQuer.removeClass("hidden");
         advBtn1.text("Hide Advanced Options");
         advShow = true;
@@ -70,45 +62,81 @@ advBtn1.click(function(e){
 
 });
 
-advBtn2.click(function(e){
-        e.preventDefault();
-        advBtn2.addClass("hidden");
-        supQuer.removeClass("hidden");
-        supShow = true;
-});
-
 //API getting
 searchBtn.click(function(e){
     e.preventDefault();
+    petContainer.empty()
 
-    var gQueryURL = "https://api.petfinder.com/v2/animals";
+    var gQueryURL = "https://api.petfinder.com/v2/animals?";
     var querys = {};
 
-    querys.type = "dog";
+    querys.type = getSpecies();
+    querys.age = getAge()
+    querys.size = getSize();
+    querys.gender = getGender();
 
-    fetch(gQueryURL + new URLSearchParams(querys), {
-        headers: {
-          Authorization: "Bearer " + apiCode
+    var otherQuers = getOther();
+    querys.good_with_children = otherQuers[0];
+    querys.good_with_dogs = otherQuers[1];
+    querys.good_with_cats = otherQuers[2];
+    querys.house_trained = otherQuers[3];
+    querys.declawed = otherQuers[4];
+    querys.special_needs = otherQuers[5];
+
+    Object.keys(querys).forEach(key => {
+        if (querys[key] === '') {
+          delete querys[key];
         }
+      });
+
+    fetch("https://api.petfinder.com/v2/oauth2/token", {
+        body: "grant_type=client_credentials&client_id=" + apiCode + "&client_secret=" + sApiCode,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST"
     })
     .then((response) => response.json())
-    .then(function (data){
-        console.log
+    .then(function (bear){
+        fetch(gQueryURL + new URLSearchParams(querys), {
+            headers: {
+                Authorization: "Bearer " + bear.access_token
+            }
+        })
+        .then((response) => response.json())
+        .then(function (data){
+            console.log(data);
+            for(i=0; i < 20; i++){
+                try{
+                    var petImage = data.animals[i].photos[0].full;
+                }
+                catch(ex){
+                    var petImage = "none";
+                }
+                var petName = data.animals[i].name;
+                var petSpecies = data.animals[i].species;
+                var petBreed = data.animals[i].breeds.primary;
+                var petId = data.animals[i].id;
+
+                CreateResult(petImage, petName, petSpecies, petBreed, petId);
+            }
+        })
     })
 })
 
 //Creates the listing
-function CreateResult (img, name, species, breed, pams){
+function CreateResult (img, name, species, breed, id){
     //initializing elements
     var linkEl = $('<a>');
     var containerEl = $('<div>');
     var nameEl = $('<h1>');
     var descEl = $('<h2>');
 
-    linkEl.html('<a href="./petPage.html?'+ pams + '"></a>');
-    linkEl.attr("href", './petPage.html?' + pams)
+    linkEl.attr("href", './petPage.html?id=' + id)
 
-    containerEl.css("background-image",'url("' + img + '")');
+    if (img != "none"){
+        containerEl.css("background-image",'url("' + img + '")');
+    }
     linkEl.append(containerEl);
 
     nameEl.text(name);
@@ -118,4 +146,111 @@ function CreateResult (img, name, species, breed, pams){
     containerEl.append(descEl);
 
     petContainer.append(linkEl)
+}
+
+function getSpecies(){
+    if ($('#Species-1').is(":checked")){
+        return "dog"
+    }
+    if ($('#Species-2').is(":checked")){
+        return "cat"
+    }
+    if ($('#Species-3').is(":checked")){
+        return "rabbit"
+    }
+    if ($('#Species-4').is(":checked")){
+        return "small & furry"
+    }
+    if ($('#Species-5').is(":checked")){
+        return "horse"
+    }
+    if ($('#Species-6').is(":checked")){
+        return "bird"
+    }
+    if ($('#Species-7').is(":checked")){
+        return "scales, fins & other"
+    }
+    if ($('#Species-8').is(":checked")){
+        return "Barnyard"
+    }
+    return "";
+}
+
+function getAge(){
+    fullString = "";
+
+    if ($('#Age-1').is(":checked")){
+        fullString += ",baby"
+    }
+    if ($('#Age-2').is(":checked")){
+        fullString += ",young"
+    }
+    if ($('#Age-3').is(":checked")){
+        fullString += ",adult"
+    }
+    if ($('#Age-4').is(":checked")){
+        fullString += ",senior"
+    }
+
+    fullString = fullString.substring(1);
+    return fullString
+}
+
+function getSize(){
+    fullString = "";
+
+    if ($('#Size-1').is(":checked")){
+        fullString += ",small"
+    }
+    if ($('#Size-2').is(":checked")){
+        fullString += ",medium"
+    }
+    if ($('#Size-3').is(":checked")){
+        fullString += ",large"
+    }
+    if ($('#Size-4').is(":checked")){
+        fullString += ",extra large"
+    }
+
+    fullString = fullString.substring(1);
+    return fullString
+}
+
+function getGender(){
+    fullString = "";
+
+    if ($('#Gender-1').is(":checked")){
+        fullString += ",male"
+    }
+    if ($('#Gender-2').is(":checked")){
+        fullString += ",female"
+    }
+    if ($('#Gender-3').is(":checked")){
+        fullString += ",Unknown"
+    }
+
+    fullString = fullString.substring(1);
+    return fullString
+}
+
+function getOther(){
+    var fullvalues = [];
+    fullvalues[0] = $('#Children').find(":selected").text();
+    fullvalues[1] = $('#Dogs').find(":selected").text();
+    fullvalues[2] = $('#Cats').find(":selected").text();
+    fullvalues[3] = $('#House').find(":selected").text();
+    fullvalues[4] = $('#Declawed').find(":selected").text();
+    fullvalues[5] = $('#Special').find(":selected").text();
+
+    for (i=0; i < 6; i++){
+        if(fullvalues[i] == 'No'){
+            fullvalues[i] = 'false';
+        } else if(fullvalues[i] == 'Yes'){
+            fullvalues[i] = 'true';
+        } else{
+            fullvalues[i] = '';
+        }
+    }
+
+    return fullvalues;
 }
